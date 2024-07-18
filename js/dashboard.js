973523606c2fd99abb4f41800af1a33da6b5be44
MedSalahArfa1 <mohamedsalaharfa@gmail.com>
2024-07-18 17:28:37 +0100

    Update template and JavaScript

--- js/dashboard.js
+++ js/dashboard.js
@@ -18,10 +18,16 @@ const db = getFirestore(app);
 document.addEventListener('DOMContentLoaded', () => {
     const logoutButton = document.getElementById('logoutButton');
     const reservationsTableBody = document.getElementById('reservationsTableBody');
+    const editForm = document.getElementById('editForm');
+    const editErrorMessage = document.getElementById('editErrorMessage');
+    const editSuccessMessage = document.getElementById('editSuccessMessage');
+    let currentEditReservationId = null;
 
     onAuthStateChanged(auth, (user) => {
         if (user) {
             fetchReservations();
+            const userEmail = user.email;
+            document.getElementById('userEmail').textContent = userEmail;
         } else {
             window.location.href = "admin.html";
         }
@@ -45,8 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     <td>${reservation.date}</td>
                     <td>${reservation.time}</td>
                     <td>
-                        <button class="btn btn-sm btn-primary edit-btn" data-id="${doc.id}">Edit</button>
-                        <button class="btn btn-sm btn-danger delete-btn" data-id="${doc.id}">Delete</button>
+                        <button class="btn btn-sm btn-light edit-btn" data-id="${doc.id}"><i class="fas fa-edit"></i></button>
+                        <button class="btn btn-sm btn-danger delete-btn" data-id="${doc.id}"><i class="fas fa-trash-alt"></i></button>
                     </td>
                 `;
                 reservationsTableBody.appendChild(row);
@@ -58,76 +64,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Event listener for edit and delete buttons
     document.addEventListener('click', async (event) => {
-        if (event.target.classList.contains('edit-btn')) {
-            const reservationId = event.target.getAttribute('data-id');
+        if (event.target.closest('.edit-btn')) {
+            const reservationId = event.target.closest('.edit-btn').getAttribute('data-id');
             const reservationRef = doc(db, 'reservations', reservationId);
             const docSnapshot = await getDoc(reservationRef);
 
             if (docSnapshot.exists()) {
                 const reservationData = docSnapshot.data();
+                currentEditReservationId = reservationId;
+
                 // Assuming you have an edit modal with id="editModal" and relevant form fields
                 const editModal = new bootstrap.Modal(document.getElementById('editModal'));
-                const editForm = document.getElementById('editForm');
-
                 editForm.name.value = reservationData.name;
                 editForm.phone.value = reservationData.phone;
                 editForm.date.value = reservationData.date;
                 editForm.time.value = reservationData.time;
 
                 editModal.show();
-
-                editForm.addEventListener('submit', async (event) => {
-                    event.preventDefault();
-
-                    const newName = editForm.name.value;
-                    const newPhone = editForm.phone.value;
-                    const newDate = editForm.date.value;
-                    const newTime = editForm.time.value;
-
-                     // Check if the selected date and time are in the past
-                     const selectedDateTime = new Date(`${newDate}T${newTime}`);
-                     const currentDateTime = new Date();
-                     if (selectedDateTime <= currentDateTime) {
-                         editErrorMessage.textContent = 'Reservations cannot be in the past.';
-                         return;
-                     }
- 
-                     // Check if the selected time is at least 30 minutes ahead of the current time
-                     const minimumTime = new Date(currentDateTime.getTime() + 30 * 60000); // 30 minutes in milliseconds
-                     if (selectedDateTime < minimumTime) {
-                         editErrorMessage.textContent = 'Please select a time at least 30 minutes ahead of the current time.';
-                         return;
-                     }
- 
-                     // Check if the selected date is a Monday (getDay() returns 1 for Monday)
-                     if (selectedDateTime.getDay() === 1) {
-                         editErrorMessage.textContent = 'Reservations cannot be made on Mondays. Please select another date.';
-                         return;
-                     }
-
-                     try {
-                        await updateDoc(reservationRef, {
-                            name: newName,
-                            phone: newPhone,
-                            date: newDate,
-                            time: newTime
-                        });
-                        editSuccessMessage.textContent = 'Reservation successfully updated.';
-                        setTimeout(() => {
-                            editSuccessMessage.textContent = '';
-                            editModal.hide();
-                            fetchReservations();
-                        }, 3000); // Clear success message and hide modal after 3 seconds
-                    } catch (error) {
-                        console.error('Error updating document: ', error);
-                        editErrorMessage.textContent = 'Failed to update reservation.';
-                    }
-                });
             }
         }
 
-        if (event.target.classList.contains('delete-btn')) {
-            const reservationId = event.target.getAttribute('data-id');
+        if (event.target.closest('.delete-btn')) {
+            const reservationId = event.target.closest('.delete-btn').getAttribute('data-id');
             try {
                 await deleteDoc(doc(db, 'reservations', reservationId));
                 console.log('Document successfully deleted');
@@ -138,4 +96,71 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
+
+    editForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const newName = editForm.name.value;
+        const newPhone = editForm.phone.value;
+        const newDate = editForm.date.value;
+        const newTime = editForm.time.value;
+
+        // Check if the selected date and time are in the past
+        const selectedDateTime = new Date(`${newDate}T${newTime}`);
+        const currentDateTime = new Date();
+        if (selectedDateTime <= currentDateTime) {
+            editErrorMessage.textContent = 'Reservations cannot be in the past.';
+            return;
+        }
+
+        // Check if the selected time is at least 30 minutes ahead of the current time
+        const minimumTime = new Date(currentDateTime.getTime() + 30 * 60000); // 30 minutes in milliseconds
+        if (selectedDateTime < minimumTime) {
+            editErrorMessage.textContent = 'Please select a time at least 30 minutes ahead of the current time.';
+            return;
+        }
+
+        // Check if the selected date is a Monday (getDay() returns 1 for Monday)
+        if (selectedDateTime.getDay() === 1) {
+            editErrorMessage.textContent = 'Reservations cannot be made on Mondays. Please select another date.';
+            return;
+        }
+
+        try {
+            const reservationRef = doc(db, 'reservations', currentEditReservationId);
+            await updateDoc(reservationRef, {
+                name: newName,
+                phone: newPhone,
+                date: newDate,
+                time: newTime
+            });
+            editSuccessMessage.textContent = 'Reservation successfully updated.';
+            setTimeout(() => {
+                editSuccessMessage.textContent = '';
+                const editModal = bootstrap.Modal.getInstance(document.getElementById('editModal'));
+                editModal.hide();
+                fetchReservations();
+            }, 3000);
+        } catch (error) {
+            console.error('Error updating document: ', error);
+            editErrorMessage.textContent = 'Failed to update reservation.';
+        }
+    });
 });
+
+(function ($) {
+    "use strict";
+
+    // Sidebar Toggler
+    $('.sidebar-toggler').click(function () {
+        $('.sidebar, .content').toggleClass("open");
+        return false;
+    });
+
+    // Calendar
+    $('#calendar').datetimepicker({
+        inline: true,
+        format: 'L'
+    });
+
+})(jQuery);
--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
+    
 })(jQuery);
 
 document.addEventListener("DOMContentLoaded", function() {
